cb81987511e979f8cb6fa0c69e81ce2875a1ccaf
Yohan <51389963+YohanWards@users.noreply.github.com>
2021-10-19 12:06:04 +1000

    Update fmriprep.sh

--- fmriprep.sh
+++ fmriprep.sh
@@ -1,4 +1,4 @@
 screen singularity run -B $HOME/scratch/tmp:/tmp \
---cleanenv my_images/fmriprep-latest.simg bids preprocessed participant \
+--cleanenv my_images/fmriprep-latest.simg bidsfunc data/derivatives/fmriprep participant \
 --t2s-coreg --use-syn-sdc --fs-license-file freesurfer.txt --output-spaces T1w MNI152NLin2009cAsym \
 fsaverage fsnative --use-aroma --skip_bids_validation --fs-no-reconall
