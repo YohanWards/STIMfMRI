91b802e3e6dd03d06a068267c083b616b6954565
Yohan <51389963+YohanWards@users.noreply.github.com>
2021-10-19 13:25:46 +1000

    Update fmriprep.sh

--- fmriprep.sh
+++ fmriprep.sh
@@ -5,7 +5,6 @@
 
 ml singularity/3.5.0
 /scratch/user/s4549155/neurocommand/local/fetch_containers.sh fmriprep 20.2.3 20210917
-ml fmriprep
 singularity run -B $HOME/scratch/tmp:/tmp \
 --cleanenv scratch/user/4549155/images/fmriprep-latest.simg STIMMRI_BIDS/STIMMRI_BIDS/ data/derivatives/fmriprep participant participant_label sub-048 \
 --t2s-coreg --use-syn-sdc --fs-license-file freesurfer.txt --output-spaces T1w MNI152NLin2009cAsym \
